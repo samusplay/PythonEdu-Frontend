1de15fd5847018e6e924341f84a01068fdbe2915
samuel <samusplay08@gmail.com>
2025-07-17 17:38:11 -0500

    ✨ PB-20: Add Footer component to main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Registro from './pages/Registro';
 function App() {
   
   return (
-   <>
+    <div>
       <Navbar />
-
-      <Routes>
+      <main>
+        <Routes>
         <Route path="/"        element={<Home />} />
         <Route path="/registro" element={<Registro />} />
       </Routes>
-    </>
+      </main>
+      <Footer />
+    </div>
   );
   
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer(){
+    return(
+        <footer className="p4-bg gray-100 text-center text-sm text-gray-600">
+        © {new Date().getFullYear()} PythonEdu. Todos los derechos reservados.
+        </footer>
+    );
+}
